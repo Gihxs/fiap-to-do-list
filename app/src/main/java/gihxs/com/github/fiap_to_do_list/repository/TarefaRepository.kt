0b1d76093ce743447be97e41362ca702809128de
Giovana Santos <giovanasilva.office@gmail.com>
2026-09-22 15:03:31 -0300

    feat: adicionar TarefaRepository e TarefaViewModel

---
+++ app/src/main/java/gihxs/com/github/fiap_to_do_list/repository/TarefaRepository.kt
@@ -0,0 +1,16 @@
+package gihxs.com.github.fiap_to_do_list.repository
+
+import gihxs.com.github.fiap_to_do_list.data.Tarefa
+import gihxs.com.github.fiap_to_do_list.data.TarefaDao
+import kotlinx.coroutines.flow.Flow
+
+class TarefaRepository(private val dao: TarefaDao) {
+
+    val tarefas: Flow<List<Tarefa>> = dao.listarTodas()
+
+    suspend fun inserir(tarefa: Tarefa) = dao.inserir(tarefa)
+
+    suspend fun atualizar(tarefa: Tarefa) = dao.atualizar(tarefa)
+
+    suspend fun deletar(tarefa: Tarefa) = dao.deletar(tarefa)
+}
