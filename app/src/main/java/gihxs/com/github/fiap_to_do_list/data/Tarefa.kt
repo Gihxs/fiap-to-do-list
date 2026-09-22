574b0d2b9209f3060270f0f60c8e7af83cffde9c
Giovana Santos <giovanasilva.office@gmail.com>
2026-09-22 15:57:22 -0300

    feat: adicionar campo dataHora e ordenar tarefas por prazo

--- app/src/main/java/gihxs/com/github/fiap_to_do_list/data/Tarefa.kt
+++ app/src/main/java/gihxs/com/github/fiap_to_do_list/data/Tarefa.kt
@@ -9,5 +9,7 @@ data class Tarefa(
     val titulo: String,
     val descricao: String,
     val concluida: Boolean = false,
-    val dataCriacao: Long = System.currentTimeMillis()
+    val dataCriacao: Long = System.currentTimeMillis(),
+    val dataHora: Long? = null
+
 )
